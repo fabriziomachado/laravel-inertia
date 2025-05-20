4740d641a7bb810a0fdcc9c9a57f4326022c4bb0
Fabrizio Machado <fabrizio.machado@gmail.com>
2025-05-20 14:59:44 -0300

    fixes nginx http

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Symfony\Component\HttpFoundation\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -21,6 +22,11 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_ALL
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
