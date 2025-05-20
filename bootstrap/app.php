a222d18b35e68d87c632240c8750e533920950e5
Fabrizio Machado <fabrizio.machado@gmail.com>
2025-05-20 15:31:04 -0300

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,10 +23,12 @@ return Application::configure(basePath: dirname(__DIR__))
             AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        $middleware->trustProxies(
-            at: '*',
-            headers: Request::HEADER_X_FORWARDED_ALL
-        );
+        $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB
+    );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
